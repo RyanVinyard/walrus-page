b69b76b998f2ea138d2b4ce0ea4ff23d6a50b808
Ryan Vinyard <Guest@epicodus-44.local>
2016-08-09 10:38:36 -0700

    Demonstrate use of fade slide and toggling

--- walrus-page/js/scripts.js
+++ walrus-page/js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $("img").show();
+  $(".clickable").click(function() {
+    $(".picture-hidden").fadeToggle("slow");
+    $(".picture-showing").fadeToggle("slow");
+  });
+});
+
+$(document).ready(function() {
+  $(".slider").click(function() {
+    $(".droptext").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".trick").click(function() {
+    $("img").fadeOut();
+    $("p").fadeOut();
+    $(".tricktext").fadeIn("slow");
   });
 });
